917b0768a3c8fb9a96f7f36b726891104d881f42
Ckotsovos <ckotsovos@gmail.com>
2015-07-03 11:24:11 -0700

    Final Code
    After good nights sleep and rational thinking, I approached everything
    wrong. So I scrapped  everything and started fresh. Previous code was
    all junk.

--- lib/class4/exercise2.rb
+++ lib/class4/exercise2.rb
@@ -17,15 +17,22 @@
 # TIP #2: Use the `string.ljust` method from class 2 exercise 7.
 #
 # TIP #3: The width of each line is 50 characters.
-hit_list = []
-hit_list << ['Can I Kick It?'      'A Tribe Called Quest'      '1991']
-hit_list << ['Rump Shaker'         'Wreckx-n-Effect'           '1992']
-hit_list << ['Check Yo Self'       'Ice Cube'                  '1993']
-hit_list << ['Regulate'            'Warren G & Nate Dogg'      '1994']
-hit_list << ['I Got 5 On It'       'Luniz'                     '1995']
-hit_list << ['Ready Or Not'        'The Fugees'                '1996']
 
-hit_list.each do |hit_list|
-  puts (hit_list) {list[5].ljust[26] + list[1].ljust[26] + list[2]}
-end
+line_width = 50
 
+hits = [
+  "90's Hip-Hop hits",
+  "Can I Kick It?      A Tribe Called Quest      1991",
+  "Rump Shaker         Wreckx-n-Effect           1992",
+  "Check Yo Self       Ice Cube                  1993",
+  "Regulate            Warren G & Nate Dogg      1994",
+  "I Got 5 On It       Luniz                     1995",
+  "Ready Or Not        The Fugees                1996"]
+
+  puts (hits[0].center(line_width))
+  puts
+  puts (hits[1].ljust(line_width/2))
+  puts (hits[2].ljust(line_width/2))
+  puts (hits[3].ljust(line_width/2))
+  puts (hits[5].ljust(line_width/2))
+  puts (hits[6].ljust(line_width/2))
